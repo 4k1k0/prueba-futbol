05ad344f3512259f1296b90a9f63083fc54ad2c6
Wako <dulo.rodrigo@gmail.com>
2019-05-13 10:14:29 -0500

    Añade funciones básicas para abrir archivos
    Se manejan los errores, se abren los archivos y se generan las cadenas
    de texto

--- archivos.go
+++ archivos.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+)
 
 func abrir(equipo, niveles string) {
-	fmt.Printf("%s ... %s", equipo, niveles)
+	err := abrirArchivo(equipo)
+	checkError(err)
+	err = abrirArchivo(niveles)
+	checkError(err)
+	equipoCadena := generaCadena(equipo)
+	nivelesCadena := generaCadena(niveles)
+	generaData(equipoCadena, nivelesCadena)
+}
+
+func abrirArchivo(archivo string) error {
+	f, err := os.Open(archivo)
+	defer f.Close()
+	return err
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func generaCadena(archivo string) string {
+	b, err := ioutil.ReadFile(archivo)
+	checkError(err)
+	s := string(b)
+	return s
+}
+
+func generaData(equipo, nivel string) {
+	fmt.Printf("%s...%s", equipo, nivel)
 }
